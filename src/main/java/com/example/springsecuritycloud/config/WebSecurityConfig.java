fbddbde2e9a10aa739cc47751b46c59a6e5825a2
shreya9598 <shreyaagupta0511@gmail.com>
2023-02-22 15:06:28 +0530

    jwt token

--- src/main/java/com/example/springsecuritycloud/config/WebSecurityConfig.java
+++ src/main/java/com/example/springsecuritycloud/config/WebSecurityConfig.java
@@ -1,47 +1,68 @@
 package com.example.springsecuritycloud.config;
 
+import com.example.springsecuritycloud.filters.JwtRequestFilter;
 import com.example.springsecuritycloud.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService);
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic();
+//        http.csrf().disable().authorizeRequests()
+////                .mvcMatchers("/**")
+////                .permitAll()
+//                .mvcMatchers(HttpMethod.GET,"/**")
+//                .hasAnyAuthority("USER","ADMIN")
+//                .mvcMatchers(HttpMethod.POST,"/**")
+//                .hasAnyAuthority("ADMIN")
+//                .anyRequest().permitAll();
+//    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception{
         http.httpBasic();
-        http.csrf().disable().authorizeRequests()
-                .mvcMatchers("/**")
-                .permitAll()
-//                .mvcMatchers(HttpMethod.GET,"/coupons/{code:{A-Z}*$}")
-//                .hasAnyRole("USER","ADMIN")
-//                .mvcMatchers(HttpMethod.POST,"/coupons")
-//                .hasAnyRole("ADMIN")
-                .anyRequest().permitAll().and();
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
 
 }
